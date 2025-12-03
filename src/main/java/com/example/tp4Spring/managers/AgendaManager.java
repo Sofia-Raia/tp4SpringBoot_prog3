7eca602f72a680412a26f17cf6452073b4a966a8
Sofia Raia <sofiaraia246@outlook.es>
2025-12-03 13:22:22 -0300

    subo readme

--- src/main/java/com/example/tp4Spring/managers/AgendaManager.java
+++ src/main/java/com/example/tp4Spring/managers/AgendaManager.java
@@ -8,18 +8,18 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//te sirve como DAO (Data Access Object) dentro del ecosistema Spring Data JPA.
+//Sirve como DAO (Data Access Object) dentro del ecosistema Spring Data JPA.
 @Repository
 public interface AgendaManager extends JpaRepository<Agenda, Long> {
 //ya trae implementados métodos CRUD y consultas básicas para la entidad
-    /*
-    disponibles métodos como:
+/*
+Dispone métodos como:
 findAll() → trae todos los registros.
 findById(Long id) → busca por id.
 save(Agenda agenda) → inserta o actualiza.
 deleteById(Long id) → elimina por id.
-
-     */
+*/
+//Query personalizada para hacer busquedas parciales.
 @Query("SELECT a FROM Agenda a WHERE LOWER(a.nombre) LIKE LOWER(CONCAT('%', :param, '%')) " +
         "OR LOWER(a.apellido) LIKE LOWER(CONCAT('%', :param, '%'))")
 List<Agenda> getItemsXNombreApellidoParcial(@Param("param") String param);
